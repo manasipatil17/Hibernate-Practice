f76d117b1f2dde4863327406f11150b842bc028b
Manasi Patil <manasipatil17052004@gmail.com>
2025-08-01 19:37:22 +0530

    added EmployeeCRUDByMethods

--- HibernateCRUDByMethods/src/main/java/com/controller/EmployeeMain.java
+++ HibernateCRUDByMethods/src/main/java/com/controller/EmployeeMain.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import java.util.Scanner;
+
 import com.entities.Employee;
 import com.service.EmpService;
 
@@ -8,14 +10,22 @@ public class EmployeeMain {
 	public static void main(String[] args) {
 		EmpService es= new EmpService();
 		Employee e=new Employee();
-		//es.insertData(e);
-		
-		//es.updateData(e);
-		
-		//es.fetchOneRow(e);
-		
-		//es.deleteData(e);
-		
-		es.fetchAllData(e);
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter button 1 to 5");
+		int btn =sc.nextInt();
+		switch(btn) {
+		case 1 :es.insertData(e);
+		break;
+		case 2 :es.updateData(e);
+		break;
+		case 3 :es.fetchOneRow(e);
+		break;
+		case 4 :es.deleteData(e);
+		break;
+		case 5 :es.fetchAllData(e);
+		break;
+		default:
+			System.exit(8);
 	}
+}
 }
