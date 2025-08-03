5037132e6e812da41994f2ee868854362d193380
Manasi Patil <manasipatil17052004@gmail.com>
2025-08-03 18:30:52 +0530

    added manytoone project

--- HibernateManyToOne/src/main/java/com/main/Main.java
+++ HibernateManyToOne/src/main/java/com/main/Main.java
@@ -1,7 +1,11 @@
 package com.main;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import com.entity.DeptManyToOne;
 import com.entity.EmpManyToOne;
 
 public class Main {
@@ -11,5 +15,31 @@ public class Main {
 		Configuration cfg=new Configuration();
 		cfg.configure();
 		cfg.addAnnotatedClass(EmpManyToOne.class);
+		cfg.addAnnotatedClass(DeptManyToOne.class);
+		
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session ss =sf.openSession();
+		Transaction ts=ss.beginTransaction();
+		
+		DeptManyToOne d=new DeptManyToOne();
+		d.setD_Id(101);
+		d.setDept_name("Java Developer");
+		
+		ss.persist(d);
+		
+		EmpManyToOne e= new EmpManyToOne();
+		e.setName("Manasi");
+	    e.setL(d);
+	    ss.persist(e);
+		EmpManyToOne e1= new EmpManyToOne();
+		e1.setName("Ram");
+		e1.setL(d);
+	 
+		
+		ss.persist(e1);
+	
+		ts.commit();
+		ss.close();
+		
 	}
 }
