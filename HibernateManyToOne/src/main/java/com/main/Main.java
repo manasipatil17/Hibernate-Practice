8bb5ab8a4ee7118bf190dae00f6a55baaa0f459c
Manasi Patil <manasipatil17052004@gmail.com>
2025-08-03 18:15:13 +0530

    added entity classes

--- HibernateManyToOne/src/main/java/com/main/Main.java
+++ HibernateManyToOne/src/main/java/com/main/Main.java
@@ -1,5 +1,15 @@
 package com.main;
 
+import org.hibernate.cfg.Configuration;
+
+import com.entity.EmpManyToOne;
+
 public class Main {
 
+	public static void main(String[] args) {
+		
+		Configuration cfg=new Configuration();
+		cfg.configure();
+		cfg.addAnnotatedClass(EmpManyToOne.class);
+	}
 }
